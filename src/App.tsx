a1aa052c16e0c8cf966bf896d5a300cea78e2a35
roscoe-patnode <rpatnode02@gmail.com>
2025-05-06 20:52:05 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Introduction from "./components/Introduction";
+import NavigationBar from "./components/NavigationBar";
+import ProjectList from "./components/ProjectList";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <Introduction />
+      <ProjectList />
+    </>
+  );
+}
+
+export default App;
